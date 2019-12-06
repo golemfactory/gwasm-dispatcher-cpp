e87fccc0c12324bb2cda1c2ad10c33ca23216a47
Adam Mizerski <amizerski@golem.network>
2019-12-06 14:19:14 +0100

    License

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,21 @@
+/*
+Copyright 2019 Golem Factory GmbH
+
+This file is part of gwasm dispatcher cpp.
+
+gwasm dispatcher cpp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gwasm dispatcher cpp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with gwasm dispatcher cpp. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define BOOST_TEST_MODULE "gwasm_dispatcher"
 #include <boost/test/unit_test.hpp>
